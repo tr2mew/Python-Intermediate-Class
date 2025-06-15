55fc026a02a303145fb28c8373793ec41ba3ad56
tr2mew <tridmew@gmail.com>
2025-06-15 16:20:34 +0700

    Damage method

--- npc.py
+++ npc.py
@@ -9,12 +9,20 @@ class NPC:
         print(f"HP: {self.hp}")
         print(f"Mana: {self.mana}")
         print("-"*20)
+        
+    def damage(self, dmg):
+        if self.mana <= 0:
+            print("not enough mana")
+            return
+        self.hp -= dmg
+        self.mana -= dmg
+        print(f"{self.name} HP got -{dmg}")
 
 n1 = NPC("Alice", 100, 200)
 n2 = NPC("Mina", 200, 100)
 
 n1.describe()
-
-
-
+n1.damage(10)
+n1.damage(10)
+n1.describe()
 n2.describe()
