a201b0557e324f188480dcdc383125759a237188
tr2mew <tridmew@gmail.com>
2025-06-15 11:32:15 +0700

    add class variable

--- Inter2/main.py
+++ Inter2/main.py
@@ -1,4 +1,7 @@
 class Student:
+    #Class variable
+    student_count = 0
+    
     def __init__(self, first_name, last_name, age, courses, salary):
         self.first_name = first_name
         self.last_name = last_name
@@ -6,6 +9,7 @@ class Student:
         self.courses = courses
         self.pocket = 0
         self.salary = salary
+        Student.student_count += 1
 
     def get_fullname(self):
         fullname = f"{self.first_name} {self.last_name}"
@@ -21,6 +25,12 @@ class Student:
 
 student1 = Student("Alice","Pottly",20, ["Math", "English"], 10000)
 student2 = Student("Bob", "Kerman",22, ["Physics", "History"], 20000)
+student3 = Student("Tim", "Bradford",36, ["Physical Education", "Science"], 30000)
 
 student1.display_info()
 student2.display_info()
+student3.display_info()
+
+student1.get_fullname()
+
+print(Student.student_count)
