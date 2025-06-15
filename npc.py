91d5d070b993ba51203fa65ec4311cc566664822
tr2mew <tridmew@gmail.com>
2025-06-15 16:10:47 +0700

    add heal method

--- npc.py
+++ npc.py
@@ -10,20 +10,15 @@ class NPC:
         print(f"Mana: {self.mana}")
         print("-"*20)
         
-    def damage(self, dmg):
-        if self.mana <= 0:
-            print("not enough mana")
-            return
-        self.hp -= dmg
-        self.mana -= dmg
-        print(f"{self.name} HP got -{dmg}")
+    def heal(self, heal):
+        self.hp += heal
 
 n1 = NPC("Alice", 100, 200)
 n2 = NPC("Mina", 200, 100)
 
 n1.describe()
-n1.damage(10)
-n1.damage(10)
+n1.heal(10)
+n1.heal(10)
 n1.describe()
 
 n2.describe()
